6d84212c20b9d7ec8a0aa4faa9f3dd9bb311ac94
Sumitkumar1109 <139613858+Sumitkumar1109@users.noreply.github.com>
2024-11-29 17:39:16 +0530

    first Commit

--- backend/mailtrap/mailtrap.config.js
+++ backend/mailtrap/mailtrap.config.js
@@ -1,8 +1,29 @@
-var transport = nodemailer.createTransport({
-    host: "live.smtp.mailtrap.io",
-    port: 587,
-    auth: {
-      user: "api",
-      pass: "786541114b2b66e374c6ba7626a37232"
-    }
-  });
+const Nodemailer = require("nodemailer");
+const { MailtrapTransport } = require("mailtrap");
+const dotenv = require("dotenv");
+
+const TOKEN = process.env.NODE_MAILER_TOKEN;
+
+const transport = Nodemailer.createTransport(
+  MailtrapTransport({
+    token: TOKEN,
+  })
+);
+
+const sender = {
+  address: "hello@demomailtrap.com",
+  name: "Mailtrap Test",
+};
+const recipients = [
+  "sumitkumar2000sep@gmail.com",
+];
+
+transport
+  .sendMail({
+    from: sender,
+    to: recipients,
+    subject: "You are awesome!",
+    text: "Congrats for sending test email with Mailtrap!",
+    category: "Integration Test",
+  })
+  .then(console.log, console.error);
